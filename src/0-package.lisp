96f03aaec5be0d841d055846b596341797c6c0fb
Masataro Asai <guicho2.71828@gmail.com>
2016-08-20 17:14:43 +0100

    more default methods and PREPARE function

--- src/0-package.lisp
+++ src/0-package.lisp
@@ -18,19 +18,23 @@
   (:documentation ""))
 
 (defgeneric measure (experiment implementation)
-  (:documentation "Wrap RUN to measure the performance and VERIFY the output.")
-  (:method (experiment implementation)
-    (with-slots (output) experiment
-       (setf output (run experiment implementation))))
-  (:method :around (experiment implementation)
-    (let ((result (call-next-method)))
-      (if (verify experiment)
-          result
-          (error "invalid results! ~a" experiment)))))
+  (:documentation "Wrap RUN to measure the performance.")
+  (:method ((e experiment) implementation)
+    (with-slots (output) e
+       (setf output (run e implementation)))))
+
+(defgeneric prepare (experiment implementation)
+  (:documentation "Prepare the input for the experiment.
+Preprocessing phase of the experiment that should be excluded from the benchmark should be here
+ rather than in RUN.")
+  (:method (e i)
+    (declare (ignorable e i))))
 
 (defgeneric run (experiment implementation)
   (:documentation "Run the experiment using an implementation.
-Returns an output."))
+Returns an output.")
+  (:method (e i)
+    (format *error-output* "benchmark for ~a and ~a is not implemented yet." e i)))
 
 (defgeneric verify (experiment)
   (:documentation
@@ -39,6 +43,9 @@ Returns an output."))
     (declare (ignore experiment))
     t))
 
-
-
-
+(defun benchmark (experiment implementation)
+  (prepare experiment implementation)
+  (let ((result (measure experiment implementation)))
+    (if (verify experiment)
+        result
+        (error "invalid results! ~a" experiment))))
