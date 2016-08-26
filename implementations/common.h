40a6abc1700baab74a6062df6b170fd87e9f5990
Masataro Asai <guicho2.71828@gmail.com>
2016-08-26 15:18:04 +0100

    added RSS measurement

--- implementations/common.h
+++ implementations/common.h
@@ -71,3 +71,4 @@ std::ostream &operator<<(std::ostream &o, const std::unordered_map<T,T2>& v){
 // std::map <string,string> measure(vector<long int> input);
 std::vector<Result> main2(std::vector<long int> input);
 
+double process_mem_usage();
