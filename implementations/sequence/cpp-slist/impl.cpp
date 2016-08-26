3ad150cfc169e4d5d0cff888a8b8074709784549
Masataro Asai <guicho2.71828@gmail.com>
2016-08-26 15:18:12 +0100

    included RSS in the results

--- implementations/sequence/cpp-slist/impl.cpp
+++ implementations/sequence/cpp-slist/impl.cpp
@@ -17,13 +17,16 @@ vector<Result> main2 (vector<long int> input) {
         Result result;
 
         Time t1 = high_resolution_clock::now();
+        double rss1 = process_mem_usage();
         for (auto i : input){
             sequence.push_front(i);
         }
+        double rss2 = process_mem_usage();
         Time t2 = high_resolution_clock::now();
 
         result[":time"] = to_string(
             (float)(duration_cast<microseconds>( t2 - t1 ).count()) / 1000000);
+        result[":space"] = to_string(rss2 - rss1);
         results.push_back(result);
     }
     
@@ -31,13 +34,16 @@ vector<Result> main2 (vector<long int> input) {
         Result result;
 
         Time t1 = high_resolution_clock::now();
+        double rss1 = process_mem_usage();
         for (auto i : input){
             sequence.pop_front();
         }
+        double rss2 = process_mem_usage();
         Time t2 = high_resolution_clock::now();
 
         result[":time"] = to_string(
             (float)(duration_cast<microseconds>( t2 - t1 ).count()) / 1000000);
+        result[":space"] = to_string(rss2 - rss1);
         results.push_back(result);
     }
     return results;
